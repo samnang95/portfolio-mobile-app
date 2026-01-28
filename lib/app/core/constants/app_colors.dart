35f2834b45ebe6b59a83ca7f505c4814fbb3b3b9
samnangsamnang048 <samnangsamnang048@gmail.com>
2026-01-28 11:53:55 +0700

    update

---
+++ lib/app/core/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/services.dart';
+
+class AppColors {
+  static const lightModeBg = Color(0xffE1F5FE);
+  static const darkModeBg = Color(0xff01579B);
+}
--- lib/app/core/constants/app_config.dart
+++ lib/app/core/constants/app_config.dart
@@ -3,4 +3,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class AppConstants {
   // API
   static String baseUrl = dotenv.get('BASE_URL');
+  static String appName = dotenv.get('APP_NAME');
 }
